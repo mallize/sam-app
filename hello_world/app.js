5bbb955c835a9abe4db8f4daa58c5fa792868967
Matt Clement <mallize@gmail.com>
2018-05-30 15:37:05 -0400

    change to use .then

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,24 +1,21 @@
 
 const axios = require('axios')
 const url = 'http://checkip.amazonaws.com/';
-let response;
-
 
 exports.lambda_handler = async (event, context, callback) => {
-    try {
-        const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                location: ret.data.trim()
+    axios(url)
+        .then(result => {
+            callback(null, {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message: 'hello world',
+                    location: ret.data.trim()
+                })
             })
-        }
-    }
-    catch (err) {
-        console.log(err);
-        callback(err, null);
-    }
+        })
+        .catch(error => {
+            console.log(error);
+            callback(error, null);
+        });
 
-    callback(null, response)
 };
